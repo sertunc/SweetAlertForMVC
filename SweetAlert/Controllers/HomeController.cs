450b503e47ebb21c3eccb1eb7acec64f293eb926
csertunc.selen <csertunc.selen@csb.gov.tr>
2017-09-29 10:48:23 +0300

    Add project files.

---
+++ SweetAlert/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace SweetAlert.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string mesaj)
+        {
+            ViewBag.SweetAlertShowMessage = SweetAlertHelper.ShowMessage("Error", "Username or Password Wrong", SweetAlertMessageType.error);
+
+            return View();
+        }
+    }
+}
---
+++ SweetAlert/SweetAlertHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace SweetAlert
+{
+    public class SweetAlertHelper
+    {
+        public static string ShowMessage(string messageCaption, string messageContent, SweetAlertMessageType messageType)
+        {
+            return $"swal('{messageCaption}','{messageContent}','{messageType}');";
+        }
+    }
+
+    public enum SweetAlertMessageType
+    {
+        warning, error, success, info
+    }
+}
